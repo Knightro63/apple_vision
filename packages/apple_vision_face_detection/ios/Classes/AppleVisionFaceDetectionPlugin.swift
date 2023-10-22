1007f01d01f3771422a9a48d6c215f8068d1fbd4
Knightro <50028636+Knightro63@users.noreply.github.com>
2023-10-22 21:26:19 +0900

    Update AppleVisionFaceDetectionPlugin.swift

--- packages/apple_vision_face_detection/ios/Classes/AppleVisionFaceDetectionPlugin.swift
+++ packages/apple_vision_face_detection/ios/Classes/AppleVisionFaceDetectionPlugin.swift
@@ -1,1 +1,1 @@
-/Users/johnsparkman/Documents/GitHub/apple_vision/packages/apple_vision_face/macos/Classes/AppleVisionFaceDetectionPlugin.swift
+/Users/johnsparkman/Documents/GitHub/apple_vision/packages/apple_vision_face_detection/macos/Classes/AppleVisionFaceDetectionPlugin.swift
