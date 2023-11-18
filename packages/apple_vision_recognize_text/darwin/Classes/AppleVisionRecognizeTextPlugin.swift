86cbbf48cf8b30eafa63956e16e580646524a8bd
alihassan143 <alihassan143cool@gmail.com>
2023-11-18 09:44:55 +0500

    revert changes in wrong file

--- packages/apple_vision_recognize_text/darwin/Classes/AppleVisionRecognizeTextPlugin.swift
+++ packages/apple_vision_recognize_text/darwin/Classes/AppleVisionRecognizeTextPlugin.swift
@@ -10,7 +10,7 @@ import AppKit
 #endif
 
 public class AppleVisionRecognizeTextPlugin: NSObject, FlutterPlugin {
-    
+
     let registry: FlutterTextureRegistry
     
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -28,102 +28,105 @@ public class AppleVisionRecognizeTextPlugin: NSObject, FlutterPlugin {
         self.registry = registry
         super.init()
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
         case "process":
             guard let arguments = call.arguments as? [String:Any],
-            let data:FlutterStandardTypedData = arguments["image"] as? FlutterStandardTypedData else {
-                result("Couldn't find image data")
-                return
+                let data:FlutterStandardTypedData = arguments["image"] as? FlutterStandardTypedData else {
+                    result("Couldn't find image data")
+                    return
             }
             let width = arguments["width"] as? Double ?? 0
             let height = arguments["height"] as? Double ?? 0
             let candidates = arguments["candidates"] as? Int ?? 1
-            #if os(iOS)
+  #if os(iOS)
                 if #available(iOS 13.0, *) {
-                    return result(convertImage(Data(data.data),CGSize(width: width , height: height),candidates,CIFormat.BGRA8))
+                    convertImage(Data(data.data), CGSize(width: width, height: height), candidates, CIFormat.ARGB8) { event in
+                result(event)
+            }
                 } else {
                     return result(FlutterError(code: "INVALID OS", message: "requires version 12.0", details: nil))
                 }
             #elseif os(macOS)
-                return result(convertImage(Data(data.data),CGSize(width: width , height: height),candidates,CIFormat.ARGB8))
+               convertImage(Data(data.data), CGSize(width: width, height: height), candidates, CIFormat.ARGB8) { event in
+                result(event)
+            }
             #endif
+           
         default:
             result(FlutterMethodNotImplemented)
         }
     }
-    
+ #if os(iOS)
+    @available(iOS 13.0, *)
+    #endif
     // Gets called when a new image is added to the buffer
-    #if os(iOS)
-    @available(iOS 13.0, *)
-    #endif
-    func convertImage(_ data: Data,_ imageSize: CGSize, _ candidates: Int,_ format: CIFormat) -> [String:Any?]{
-        let imageRequestHandler:VNImageRequestHandler
-        if data.count == (Int(imageSize.height)*Int(imageSize.width)*4){
-            // Create a bitmap graphics context with the sample buffer data
-            let context =  CIImage(bitmapData: data, bytesPerRow: Int(imageSize.width)*4, size: imageSize, format: format, colorSpace: nil)
-            
-            imageRequestHandler = VNImageRequestHandler(ciImage:context,orientation: .downMirrored)
-        }
-        else{
-            imageRequestHandler = VNImageRequestHandler(
-                data: data,
-                orientation: .down)
-        }
-            
-        var event:[String:Any?] = ["name":"noData"];
+    func convertImage(_ data: Data, _ imageSize: CGSize, _ candidates: Int, _ format: CIFormat, completion: @escaping ([String: Any?]) -> Void) {
+        DispatchQueue.global().async {
+            let imageRequestHandler: VNImageRequestHandler
+            if data.count == (Int(imageSize.height) * Int(imageSize.width) * 4) {
+                // Create a bitmap graphics context with the sample buffer data
+                let context = CIImage(bitmapData: data, bytesPerRow: Int(imageSize.width) * 4, size: imageSize, format: format, colorSpace: nil)
 
-        do {
-            try
-            imageRequestHandler.perform([VNRecognizeTextRequest { (request, error)in
-                if error == nil {
-                    
-                    if let results = request.results as? [VNRecognizedTextObservation] {
-                        var listText:[[String:Any?]] = []
-                        for text in results {
-                            listText.append(self.processObservation(text,imageSize,candidates))
-                        }
-                        event = [
-                            "name": "recognizeText",
-                            "data": listText,
-                            "imageSize": [
-                                "width": imageSize.width,
-                                "height": imageSize.height
+                imageRequestHandler = VNImageRequestHandler(ciImage: context, orientation: .downMirrored)
+            } else {
+                imageRequestHandler = VNImageRequestHandler(
+                    data: data,
+                    orientation: .down)
+            }
+
+            var event: [String: Any?] = ["name": "noData"]
+
+            do {
+                try imageRequestHandler.perform([VNRecognizeTextRequest { (request, error) in
+                    if error == nil {
+
+                        if let results = request.results as? [VNRecognizedTextObservation] {
+                            var listText: [[String: Any?]] = []
+                            for text in results {
+                                listText.append(self.processObservation(text, imageSize, candidates))
+                            }
+                            event = [
+                                "name": "recognizeText",
+                                "data": listText,
+                                "imageSize": [
+                                    "width": imageSize.width,
+                                    "height": imageSize.height
+                                ]
                             ]
-                        ]
+                        }
+                    } else {
+                        event = ["name": "error", "code": "No Text Detected", "message": error!.localizedDescription]
+                        print(error!.localizedDescription)
                     }
-                } else {
-                    event = ["name":"error","code": "No Text Detected", "message": error!.localizedDescription]
-                    print(error!.localizedDescription)
-                }
-            }])
-        } catch {
-            event = ["name":"error","code": "Data Corropted", "message": error.localizedDescription]
-            print(error)
+                    completion(event) // Call the completion handler when processing is complete
+                }])
+            } catch {
+                event = ["name": "error", "code": "Data Corropted", "message": error.localizedDescription]
+                print(error)
+                completion(event) // Call the completion handler even in case of an error
+            }
         }
-
-        return event;
     }
-    
-    #if os(iOS)
+ #if os(iOS)
     @available(iOS 13.0, *)
     #endif
-    func processObservation(_ observation: VNRecognizedTextObservation,_ imageSize: CGSize, _ candidates: Int) -> [String:Any?] {
+    func processObservation(_ observation: VNRecognizedTextObservation, _ imageSize: CGSize, _ candidates: Int) -> [String: Any?] {
         // Retrieve all torso points.
         let recognizedPoints = observation.boundingBox
-        let coord =  VNImagePointForNormalizedPoint(recognizedPoints.origin,
-                                             Int(imageSize.width),
-                                             Int(imageSize.height))
+        let coord = VNImagePointForNormalizedPoint(recognizedPoints.origin,
+                                                   Int(imageSize.width),
+                                                   Int(imageSize.height))
         return [
-            "minX":Double(recognizedPoints.minX),
-            "maxX":Double(recognizedPoints.maxX),
-            "minY":Double(recognizedPoints.minY),
-            "maxY":Double(recognizedPoints.maxX),
-            "height":Double(recognizedPoints.height),
-            "width":Double(recognizedPoints.width),
-            "origin": ["x":(imageSize.width-coord.x)-(recognizedPoints.width*imageSize.width),"y":coord.y],
-            "text": observation.topCandidates(candidates).map {  text in
+            "minX": Double(recognizedPoints.minX),
+            "maxX": Double(recognizedPoints.maxX),
+            "minY": Double(recognizedPoints.minY),
+            "maxY": Double(recognizedPoints.maxX),
+            "height": Double(recognizedPoints.height),
+            "width": Double(recognizedPoints.width),
+            "origin": ["x": (imageSize.width - coord.x) - (recognizedPoints.width * imageSize.width), "y": coord.y],
+            "text": observation.topCandidates(candidates).map { text in
                 text.string
             }
         ]
